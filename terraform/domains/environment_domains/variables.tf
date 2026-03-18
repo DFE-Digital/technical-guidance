669efb3094f90981322a18ca07a68da6a61c1cf4
Shahe Islam <shahe.islam@education.gov.uk>
2026-03-18 17:15:31 +0000

    Spike: Front Door WAF rules to block crawling on invalid paths
    Add block_paths variable to environment_domains module that creates
    custom WAF rules to block requests matching specified URL patterns
    (e.g. .php, .asp, /wp-admin).
    
    Review config included with placeholder origin hostname.
    
    Ticket: #2613

--- terraform/domains/environment_domains/variables.tf
+++ terraform/domains/environment_domains/variables.tf
@@ -2,3 +2,18 @@ variable "hosted_zone" {
   type    = map(any)
   default = {}
 }
+
+variable "block_paths" {
+  type = list(object({
+    name       = string
+    priority   = number
+    enabled    = optional(bool, true)
+    patterns   = list(string)
+    operator   = optional(string, "EndsWith")
+    transforms = optional(list(string), ["Lowercase"])
+    negate     = optional(bool, false)
+  }))
+  nullable    = false
+  default     = []
+  description = "List of path blocking rules for Front Door WAF"
+}
